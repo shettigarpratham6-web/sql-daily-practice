b15d9001bee0f68878cc9bff4e5a30dcd5fee211
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-09-04 20:31:08 +0530

    Add files via upload
    basic creation of table and inserting the values to it

---
+++ SQL_learn_1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Students (
+    StudentID INT ,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Age INT,
+    Email VARCHAR(100)
+);
+
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Email)
+VALUES (1, 'John', 'Doe', 20, 'john.doe@gmail.com');
+
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Email)
+VALUES (2, 'Emma', 'Smith', 22, 'emma.smith@gmail.com');
+
+INSERT INTO Students (StudentID, FirstName, LastName, Age, Email)
+VALUES (3, 'Raj', 'Kumar', 19, 'raj.kumar@gmail.com');
+
+
